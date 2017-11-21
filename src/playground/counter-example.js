be32db9b51471ae508dfeb14a16d0360f324f58b
fabianobrancher <fabiano.brancher@hotmail.com>
2017-11-20 23:14:18 -0200

    Counter-example refactored to stateless functional component

--- src/playground/counter-example.js
+++ src/playground/counter-example.js
@@ -1,5 +1,4 @@
 class Counter extends React.Component {
-<<<<<<< HEAD
     constructor(props) {
         super(props);
         this.handleAddOne = this.handleAddOne.bind(this);
@@ -112,48 +111,3 @@ ReactDOM.render(<Counter count={5}/>, document.getElementById('app'));
 // };
 
 // renderCounterApp();
-=======
-  constructor(props) {
-    super(props);
-    this.handleAddOne = this.handleAddOne.bind(this);
-    this.handleMinusOne = this.handleMinusOne.bind(this);
-    this.handleReset = this.handleReset.bind(this);
-    this.state = {
-      count: 0
-    };
-  }
-  handleAddOne() {
-    this.setState((prevState) => {
-      return {
-        count: prevState.count + 1
-      };
-    });
-  }
-  handleMinusOne() {
-    this.setState((prevState) => {
-      return {
-        count: prevState.count - 1
-      };
-    });
-  }
-  handleReset() {
-    this.setState(() => {
-      return {
-        count: 0
-      };
-    });
-  }
-  render() {
-    return (
-      <div>
-        <h1>Count: {this.state.count}</h1>
-        <button onClick={this.handleAddOne}>+1</button>
-        <button onClick={this.handleMinusOne}>-1</button>
-        <button onClick={this.handleReset}>reset</button>
-      </div>
-    );
-  }
-}
-
-ReactDOM.render(<Counter />, document.getElementById('app'));
->>>>>>> c1d7680fcf66b11afe5dec348d842b20ab48844f
