8e173fef96afb27d9010c33f2e01a5467d8cdc6c
fabianobrancher <fabiano.brancher@hotmail.com>
2018-01-04 22:47:40 -0200

    styling header

---
+++ src/components/Action.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+//stateless functional component
+const Action = (props) => (
+  <div>
+    <button onClick={props.handlePick} disabled={!props.hasOptions}>
+      What should I do?
+    </button>
+  </div>
+);
+  
+  // class Action extends React.Component {
+  //   render() {
+  //     return (
+  //       <div>
+  //         <button onClick={this.props.handlePick} disabled={!this.props.hasOptions}> 
+  //           What should I do? 
+  //         </button>
+  //       </div>
+  //     )
+  //   }
+  // }
+
+  export default Action;
