ef7fd609fa746b1c8c1412be14d982bd2f2f0846
Fabiano <fabiano.brancher@hotmail.com>
2018-01-05 00:01:17 -0200

    styling

--- src/app.js
+++ src/app.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import IndecisionApp from './components/IndecisionApp';
-import './styles/styles.scss';
 
-ReactDOM.render(<IndecisionApp />, document.getElementById('app'));
+const template = <p> this is jsx from webpack </p>
+ReactDOM.render(template, document.getElementById('app'));
