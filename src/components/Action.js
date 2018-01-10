e2911f9d41375562224610985299344dbcc6209b
fabianobrancher <fabiano.brancher@hotmail.com>
2018-01-10 19:35:46 -0200

    Final Version of IndecisionApp

--- src/components/Action.js
+++ src/components/Action.js
@@ -3,8 +3,11 @@ import React from 'react';
 //stateless functional component
 const Action = (props) => (
   <div>
-    <button onClick={props.handlePick} disabled={!props.hasOptions}>
-      What should I do?
+    <button 
+      className="big-button"  
+      disabled={!props.hasOptions} 
+      onClick={props.handlePick} > 
+        What should I do?
     </button>
   </div>
 );
--- src/components/Options.js
+++ src/components/Options.js
@@ -3,16 +3,30 @@ import Option from './Option';
 
 const Options = (props) => (
   <div>
-    <button onClick={props.handleDeleteOptions}> Remove All </button>
-    {props.options.length === 0 && <p> Please add an option to get started! </p>}
-      {
-        //this.props.options.map((option) => <p key={option}> {option} </p>)
-        props.options.map((option) => (
-          <Option key={option} optionText={option} handleDeleteOption={props.handleDeleteOption} />
-        ))
-      }
+    <div className="widget-header">
+      <h3 className="widget-header__title"> Your Options </h3>
+      <button 
+        className="button button--link"
+        onClick={props.handleDeleteOptions}
+      > 
+        Remove All 
+      </button>
     </div>
-  );
+
+    {props.options.length === 0 && <p className="widget-header__message"> Please add an option to get started! </p>}
+        {
+          //this.props.options.map((option) => <p key={option}> {option} </p>)
+          props.options.map((option, index) => (
+            <Option 
+              key={option} 
+              optionText={option}
+              count={index + 1}
+              handleDeleteOption={props.handleDeleteOption} 
+            />
+          ))
+        }
+  </div>
+);
 
   
   // class Options extends React.Component {
